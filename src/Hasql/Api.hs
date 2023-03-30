b624752699279d2525f65ec58c4300bc3f0a8ec4
Michal Kleczek <michal.kleczek@sap.com>
2023-03-30 21:40:53 +0200

    Added effect

--- src/Hasql/Api.hs
+++ src/Hasql/Api.hs
@@ -2,7 +2,7 @@ module Hasql.Api (Sql (..), RunnableSql (..)) where
 
 import Data.ByteString as B
 
-class Sql s m | m -> s where
+class Sql s m where
   sql :: B.ByteString -> m ()
   statement :: parameters -> s parameters result -> m result
 
---
+++ src/Hasql/Api/Eff.hs
@@ -0,0 +1,40 @@
+module Hasql.Api.Eff where
+
+import Data.ByteString (ByteString)
+import Effectful
+import Effectful.Dispatch.Dynamic (interpret, localSeqUnliftIO, send)
+import Effectful.Error.Static (Error, throwError)
+import Effectful.Reader.Static (Reader, ask)
+import Hasql.Api
+import Hasql.Connection (Connection)
+import Hasql.Session (QueryError, run, sql, statement)
+import Hasql.Statement (Statement)
+
+data ParametrizedSql s :: Effect where
+  Statement :: params -> s params result -> ParametrizedSql s m result
+
+data SimpleSql q :: Effect where
+  Sql :: q -> SimpleSql q m ()
+
+type instance DispatchOf (ParametrizedSql s) = 'Dynamic
+type instance DispatchOf (SimpleSql s) = 'Dynamic
+
+type SqlSession q s es = (ParametrizedSql s :> es, SimpleSql q :> es)
+
+instance (SqlSession ByteString s es) => Sql s (Eff es) where
+  sql = send . Sql
+  statement parms stmt = send $ Statement parms stmt
+
+runSimple :: (IOE :> es, Reader Connection :> es, Error QueryError :> es) => Eff (SimpleSql ByteString : es) a -> Eff es a
+runSimple = interpret $ \env (Sql query) -> do
+  c <- ask
+  res <- localSeqUnliftIO env $ \_ -> do
+    Hasql.Session.run (Hasql.Session.sql query) c
+  either throwError pure res
+
+runParametrized :: (IOE :> es, Reader Connection :> es, Error QueryError :> es) => Eff (ParametrizedSql Statement : es) a -> Eff es a
+runParametrized = interpret $ \env (Statement params stmt) -> do
+  c <- ask
+  res <- localSeqUnliftIO env $ \_ -> do
+    Hasql.Session.run (Hasql.Session.statement params stmt) c
+  either throwError pure res
